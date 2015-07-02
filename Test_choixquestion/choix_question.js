99d0d511f3d8734f06d6465c1ae4e1f52f6cd720
JulienOuvrard <julien.ouvrard311@gmail.com>
2015-07-02 10:59:33 +0200

    update du choix et ajout de la librairie natural

--- Test_choixquestion/choix_question.js
+++ Test_choixquestion/choix_question.js
@@ -63,6 +63,17 @@ function getTime(tab){
     return ret;
 }
 
+function getEnonce(tab){
+    var ret={};
+    var ann = tab.annotations;
+    ann.forEach(function(elem){
+        if(elem.type == "Quizz"){
+            ret[elem.id] = elem.content.description;
+        }
+    });
+    return ret;
+}
+
 function positive(tab){
     var votted_properties = ["usefull","useless","skipped_vote"];
     var res={};
@@ -78,10 +89,83 @@ function positive(tab){
     return res;
 }
 
-function similaire(q1,q2){
-   
-    return (Math.abs((time[q1] - time[q2])) < _delta_time)
+function arrayUnique(array) {
+    var a = array.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
 
+    return a;
+}
+
+function dot(v1,v2){
+    var res = 0;
+    for(var i = 0;i < v1.length;i++){
+        res+= (v1[i]*v2[i]);
+    }
+    return res;
+}
+
+function norm(v){
+    var res = 0, temp=0;
+    v.forEach(function(elem){
+        temp+=elem*elem
+    });
+    res=Math.sqrt(temp);
+    return res;
+}
+
+function cosine(v1,v2){
+    return (dot(v1,v2)/(norm(v1)*norm(v2)));
+}
+
+function syntax_similarity(phrase1,phrase2){
+    
+    natural.PorterStemmerFr.attach();
+    p1=phrase1.tokenizeAndStem();
+    p2=phrase2.tokenizeAndStem();
+
+    //console.log(p1);
+    //console.log(p2);
+
+    var words=arrayUnique(p1.concat(p2));
+
+    //console.log(words);
+
+    var TfIdf = natural.TfIdf, tfidf = new TfIdf();
+    tfidf.addDocument(p1);
+    tfidf.addDocument(p2);
+    var t1 =[];
+    
+    var j = 0;
+    words.forEach(function(elem){
+        var temp = [];
+        tfidf.tfidfs(elem, function(i, measure) {
+            temp[i]=measure;
+        });
+        t1[j]=temp;
+        ++j;
+    })
+    
+    //console.log(t1);
+    var cos = [];
+    $.each(t1, function(index, value) {
+        var temp = $.extend(true, {}, t1);
+        delete temp[index];
+        $.each(temp, function(index2, value2) {
+            //console.log(cosine(value,value2));
+            cos.push(cosine(value,value2));
+        });
+    }); 
+    console.log(cos);
+    //return {p1,p2,t1};
+}
+
+function time_similarity(q1,q2){
+    return (Math.abs((time[q1] - time[q2])) < _delta_time);
 }
 
 function getData(data1,data2){
@@ -110,19 +194,29 @@ function getData(data1,data2){
     time = getTime(data2[0]);
     //console.log(time);
     
-    $("#content").append("<ul>");
+    //console.log('Enoncés : ');
+    enonces = getEnonce(data2[0]);
+    //console.log(enonces);
+    
+    //$("#content").append("<ul>");
     $.each(time, function(index, value) {
-        $("#content").append("<li>"+index+" : ");
+        //$("#content").append("<li>"+index+" : ");
         var temp = $.extend(true, {}, time);
         delete temp[index];
         $.each(temp, function(index2, value2) {
-            if(similaire(index,index2)){
-                $("#content").append("<ul><li>"+index2+"</li></ul>");
+            if(time_similarity(index,index2)){
+                //$("#content").append("<ul><li>"+index2+"</li></ul>");
+                console.log(index2);
             }
+            /*if(syntax_similarity(enonces[index],enonces[index2])){
+                console.log(enonces[index]+' | '+enonces[index2]);
+            }*/
+            syntax_similarity(enonces[index],enonces[index2]);
         });
-        $("#content").append("</li>")
+        //$("#content").append("</li>")
     }); 
-    $("#content").append("</ul>");
+    //$("#content").append("</ul>");
+    
 
 };
 
@@ -130,4 +224,3 @@ function getData(data1,data2){
 //url_data : http://comin-ocw.org/devpf/api/annotations/
 
 $.when($.get("analytics.json"), $.get("data.json")).done(function(dat1,dat2){getData(dat1,dat2)});
-

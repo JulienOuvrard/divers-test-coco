1347cd25e1ff29fce99ac26cc51f9fd188d7ab3e
JulienOuvrard <julien.ouvrard311@gmail.com>
2015-07-03 15:49:03 +0200

    finalisation simulateur

--- Simulateur_question/app.js
+++ Simulateur_question/app.js
@@ -1,100 +1,126 @@
-function getFrequency(arr){
+function pickRandomNumber(min,max){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getStopWords(data1,data2){
+    return data1[0].split("\n").concat(data2[0].split("\n")).map(function(s){return s.trim()});
+}
+
+function cleaningText(str){
+    return str.toLowerCase()
+                .replace(//g," ")
+                .replace(/\n/g," ")
+                .replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()\?@]/g," ")
+                .replace(/[\d]/g," ");
+}
+
+function getDonnees(data){
+    var ret = [];
+    var annotations = data[0].annotations;
+    annotations.forEach(function(elem,index){
+        if(elem.meta["id-ref"] == "Slides"){
+            var donnee = {};
+            donnee.deb = elem.begin;
+            donnee.fin = elem.end;
+            var txt = elem.content.description;
+            var mots = cleaningText(txt).split(" ");
+            donnee.texte = mots ;
+            ret.push(donnee);
+        }
+    });
+    return ret
+}
+
+function filterStopWords(str){
+    return !($.inArray(str,this) > -1);
+}
+
+function filtering(d,stpw){
+    $.each(d,function(index,value){
+        var tab_str = value.texte;
+        var new_tab = tab_str.filter(filterStopWords,stpw);
+        value.texte=new_tab;
+    });
+}
+
+function getFrequencies(arr){
     var ret = {};
     $.each(arr, function(index,value){
         ret[value] = ret[value] ? ret[value]+1 : 1;
     });
     return ret;
 }
-function generate(arr,nombre,longmin,longmax){
-    var retour={};
-    for(var n=0; n<nombre; n++){
-        var quest= {}, enonce = "" , reps = {};
-        var long = Math.floor(Math.random() * (longmax - longmin)) + longmin;
-        var nbAns = Math.floor(Math.random() * 4) + 2;
-        for(var i=0; i<=long; i++){
-            var index = Math.floor(Math.random() * (arr.length));
-            enonce+= arr[index]+" ";
-        }
-        quest.desc=enonce;
-        for(var j=0; j<nbAns; j++){
-            var rep="";
-            for(var k=0 ;k < long ;k++){
-                var index2 = Math.floor(Math.random() * (arr.length));
-                rep+= arr[index2]+" ";
-            }
-            reps[j]=rep;
-        }
-        quest.ans=reps;
-        retour[n]=quest;
+
+function generateTime(obj){
+    return pickRandomNumber(obj.deb,obj.fin);
+}
+
+function generateTxt(tab,l_min,l_max){
+    var txt="";
+    var long = pickRandomNumber(l_min,l_max);
+    for(var i=0; i<=long; i++){
+        var index = pickRandomNumber(0,tab.length);
+        txt+= tab[index]+" ";
     }
+    return txt;
+}
+
+function generateQuestion(tab_mots,longMin,longMax,nbRepMin,nbRepMax){
+    var question = {};
+    var description = generateTxt(tab_mots,longMin,longMax);
+    var answers = {};
+    var nbRep = pickRandomNumber(nbRepMin,nbRepMax);
+    for(var i = 0; i < nbRep; i++){
+        var rep = generateTxt(tab_mots,longMin,longMax);
+        answers[i] = rep;
+    }
+    question.enonce = description;
+    question.reponses = answers;
+    return question;
+}
+
+function generate(tab,nombre,longMin,longMax,nbRepMin,nbRepMax){
+    var retour={};
+    for(var i = 0 ; i < nombre ; i++){
+       var idx = pickRandomNumber(0,tab.length);
+       var obj = tab[idx];
+       var obj_av = tab[idx-1];
+       var obj_ap = tab[idx+1];
+       var time = generateTime(obj);
+       var mots; 
+       var t1 = obj.texte, t2 = obj_av.texte, t3 = obj_ap.texte;
+       if(idx > 0 && idx < tab.length){
+           mots = t2.concat(t1).concat(t3);
+       }else if (idx == 0){
+           mots = t1.concat(t3);
+       }else{
+           mots = t1.concat(t2);
+       }
+       var quest =  generateQuestion(mots,longMin,longMax,nbRepMin,nbRepMax);
+       quest.time = time;
+       retour[i] = quest;
+    }
+
     return retour;
 }
 
-renameProperty = function (obj, oldName, newName) {
-     // Do nothing if the names are the same
-     if (oldName == newName) {
-         return obj;
-     }
-    // Check for the old property name to avoid a ReferenceError in strict mode.
-    if (obj.hasOwnProperty(oldName)) {
-        obj[newName] = obj[oldName];
-        delete obj[oldName];
-    }
-    return obj;
-};
-
-function cleaningText(str){
-    return str.toLowerCase().replace(//g," ").replace( /\n/g, " " );
+function main(d1,d2,d3){
+    
+    var stopwords_fr = getStopWords(d1,d2);
+    //console.table(stopwords_fr);
+    
+    var donnees = getDonnees(d3);
+    
+    filtering(donnees,stopwords_fr);
+    //console.table(donnees);
+    
+    var questions =  generate(donnees,40,5,10,2,5);
+    console.table(questions);
+    
 }
 
-$.ajax({
-    url : "data.json",
-    dataType: "json",
-    success : function (data) {
-        var donnees = [];
-        var annotations = data.annotations;
-        annotations.forEach(function(elem,index){
-            if(elem.meta["id-ref"] == "Slides"){
-                var donnee = {};
-                donnee.deb = elem.begin;
-                donnee.fin = elem.end;
-                var txt = elem.content.description;
-                var mots = cleaningText(txt).split(" ");
-                donnee.texte = mots ;
-                donnees.push(donnee);
-            }
-        });
-        console.table(donnees);
-        
-        
-        /*
-        var mots = cleaningText(data).split(" ");
-        console.table(mots);
-        
-        var freq = getFrequency(mots);
-        console.table(freq);
-        
-        var questions = generate(mots,5,5,10);
-        console.table(questions);
-        
-        var a = ["","ce","le"];
-        var test = ["","petit","le","jour","avec","le","soir","coucou","re","tard","dos","mama","huhuu","crie","bobo","","ceci","clea","","ce","le","","ce","le","poeur","ce","cae","","ce","mo","","ce","le","","ce","matin","nate","ce","le","","quo","le","bnbn","nbnv","ccsd","","pcra","lib","","pu","sans"];
-        console.log(test);
-        
-        var cop = $.extend(true, {}, test);
-        
-        $.each(test,function(index,value){
-            if($.inArray(value,a) > -1){
-                delete cop[index];
-            }
-        });
-        //console.log(Object.keys(cop));
-        var old_keys=Object.keys(cop);
-        for(var i = 0 ; i< cop.length; i++){
-            cop.renameProperty(old_keys[i],i);
+$.when($.get("stop-words_french_1_fr.txt"), $.get("stop-words_french_2_fr.txt"), $.get("data.json"))
+    .done(function(data1,data2,data3){
+        main(data1,data2,data3);
         }
-        
-        console.log(cop);
-        */
-    }
-});
+    );
